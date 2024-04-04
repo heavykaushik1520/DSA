0b9648a4887535ff93161d9962368a4c4ce0dff4
Kaushik <kaushikmandavkar6@gmail.com>
2024-04-04 23:55:41 +0530

    String pallindrome

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,25 +1,23 @@
 function bubbleSort(arr, n) {
-    var i, j, temp;
-    var swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = true;
-            }
-        }
-        if (swapped == false)
-            break;
+  var i, j, temp;
+  var swapped;
+  for (i = 0; i < n - 1; i++) {
+    swapped = false;
+    for (j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
     }
+    if (swapped == false) break;
+  }
 }
 
 function printArray(arr, size) {
-    var i;
-    for (i = 0; i < size; i++)
-        console.log(arr[i] + " ");
+  var i;
+  for (i = 0; i < size; i++) console.log(arr[i] + " ");
 }
 
 var arr = [64, 34, 25, 12, 22, 11, 90];
